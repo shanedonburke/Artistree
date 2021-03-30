f7c445278c7acb9fe5c67b8663ba7477e4c0f186
Jacob Waclawski <54681308+jmw3638@users.noreply.github.com>
2021-03-30 18:48:11 -0400

    Added reactstrap

--- src/Artistree/NavBar.js
+++ src/Artistree/NavBar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Modal, ModalHeader, ModalBody, ModalFooter
+} from 'reactstrap'
 
 import Logo from './artistree_logo.png';
 import './NavBar.css';
@@ -7,7 +9,10 @@ import './NavBar.css';
 class NavBar extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = { 
+            hamburgerOpen: false,
+            modalOpen: false,
+        };
     }
 
     render() {
@@ -19,7 +24,24 @@ class NavBar extends React.Component {
                     <Link to="/canvas"><span className="navbar-link">Canvas</span></Link>
                     <Link to="/my-submissions"><span className="navbar-link">My Submissions</span></Link>
                 </div>
-                <span className="material-icons hamburger-icon">menu</span>
+                <div>
+                    <Modal isOpen={this.state.modalOpen} toggle={() => this.setState({modalOpen: !this.state.modalOpen})}>
+                        <ModalHeader>Hello</ModalHeader>
+                        <ModalBody>World</ModalBody>
+                    </Modal>
+                </div>
+                <div>
+                    <Dropdown isOpen={this.state.hamburgerOpen} toggle={() => this.setState({hamburgerOpen: !this.state.hamburgerOpen})}>
+                        <DropdownToggle tag="span" data-toggle="dropdown" aria-expanded={this.state.hamburgerOpen}>
+                            <span className="material-icons hamburger-icon">menu</span>
+                        </DropdownToggle>
+                        <DropdownMenu>
+                            <DropdownItem onClick={() => this.setState({modalOpen: true})}>Login</DropdownItem>
+                            <DropdownItem>Logout</DropdownItem>
+                            <DropdownItem>Account</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                </div>
             </div>
         );
     }
