7c58a37261d6dee32b98cb8d66ba1a1fefb29341
Jacob Waclawski <54681308+jmw3638@users.noreply.github.com>
2021-03-30 19:39:26 -0400

    Gallery posts

--- src/Artistree/Gallery.js
+++ src/Artistree/Gallery.js
@@ -1,15 +1,39 @@
 import React from 'react';
 
+import GalleryPost from './GalleryPost';
+
+import './Gallery.css';
+
 class Gallery extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = { grid: [] };
+    }
+
+    buildGrid(size) {
+        const grid = [];
+        for (let i = 0; i < size; i++) grid.push(i);
+        return grid;
+    }
+
+    componentDidMount () {
+        const grid = this.buildGrid(10);
+        this.setState({grid: grid});
     }
 
     render() {
         return (
-            <div>
-                Gallery
+            <div className="gallery-root">
+                <div className="title-bar">
+                    <div>Gallery</div>
+                </div>
+                <div className="page-body">
+                    {Array.from(this.state.grid).map((idx) => {
+                        return (
+                            <GalleryPost key={idx}>{idx}</GalleryPost>
+                        );
+                    })}
+                </div>
             </div>
         );
     }
