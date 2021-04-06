b388725eef07109100ec9ae1af53e9714a86c59f
Saadman <st7941@g.rit.edu>
2021-04-06 16:41:19 -0400

    Modal initialized but not visible on screen

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ import Canvas from './Artistree/Canvas/Canvas';
 import MySubmissions from './Artistree/MySubmissions';
 import LeavingFeedback from './Artistree/LeavingFeedback/LeavingFeedback';
 import NavBar from './Artistree/NavBar';
+import Modal from 'react-modal';
+import { useState } from 'react';
 
 function App() {
+  const [modalIsOpen, setModalIsOPen] = useState(false)
   return (
     <div className="App">
+      <button onClick = {() => setModalIsOPen(true)}>Report</button>
+      <Modal isOpen={modalIsOpen}>
+        <h2>
+          Reason for Reporting
+        </h2>
+        <p>
+          1.
+          2.
+          3.
+        </p>
+      </Modal>
       <Router>
         <NavBar />
         <Switch>
