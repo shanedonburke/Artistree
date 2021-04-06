75b1c793e63ee1c5c292ccbe17db4583c3e1cfd5
shanedonburke <shanedonburke@gmail.com>
2021-04-06 15:42:23 -0400

    Refine feedback page

--- src/Artistree/LeavingFeedback/LeavingFeedback.js
+++ src/Artistree/LeavingFeedback/LeavingFeedback.js
@@ -1,101 +1,189 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import { useLocation } from "react-router-dom";
 import { TextField, Divider, Avatar, Grid, Paper } from "@material-ui/core";
 
-
-import './LeavingFeedback.css'
+import "./LeavingFeedback.css";
+import { Button } from "reactstrap";
 const imgLink =
-    "https://cf-images.us-east-1.prod.boltdns.net/v1/static/854081161001/e7c0a8f2-4b0b-40e7-858f-47de21dc8dcb/794143eb-4285-434e-8a08-820dab64c65b/1280x720/match/image.jpg";
-const imgLink2 = "https://preview.redd.it/cfd0qlpofcb01.jpg?auto=webp&s=b2b0b996ec334bcddc3088037130e9dba186ded0";
+  "https://cf-images.us-east-1.prod.boltdns.net/v1/static/854081161001/e7c0a8f2-4b0b-40e7-858f-47de21dc8dcb/794143eb-4285-434e-8a08-820dab64c65b/1280x720/match/image.jpg";
+const imgLink2 =
+  "https://preview.redd.it/cfd0qlpofcb01.jpg?auto=webp&s=b2b0b996ec334bcddc3088037130e9dba186ded0";
 const imgLink3 = "https://i.redd.it/kc9sfn0jgnwz.jpg";
 
 export default function LeavingFeedback() {
-    const { state } = useLocation();
+  const { state } = useLocation();
 
-    return (
-        <div className="LeavingFeedback-root">
-            <div className="title-bar">
-                <div>Critique</div>
+  return (
+    <div className="LeavingFeedback-root">
+      <div className="title-bar">
+        <div>Critique</div>
+      </div>
+
+      <div className="LeavingFeedback-page-body">
+        <div style={{ height: "100%", display: "flex" }}>
+          <Grid
+            container
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              flexWrap: "nowrap",
+            }}
+          >
+            <Paper
+              style={{
+                width: "auto",
+                height: "auto",
+                textAlign: "left",
+                marginRight: "200px",
+              }}
+            >
+              <img
+                src={state.image}
+                alt="dynamo"
+                style={{
+                  width: "600px",
+                  height: "100%",
+                }}
+              />
+            </Paper>
+
+            <div style={{ height: "100%", width: "100%", overflow: "hidden" }}>
+              <Paper
+                style={{
+                  padding: "10px 20px",
+                  width: "100%",
+                  height: "100%",
+                }}
+              >
+                <h4>
+                  Critique this artwork. Be sure to follow the Critiquing
+                  Guidelines!
+                </h4>
+
+                <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+                  <div style={{ margin: "20px 0" }}>
+                    <TextField
+                      id="standard-basic"
+                      label="Leave a Critique"
+                      multiline
+                      rows={4}
+                      variant="outlined"
+                      fullWidth
+                    />
+                    <Button
+                      color="primary"
+                      style={{ marginTop: "10px", float: "right" }}
+                    >
+                      Submit
+                    </Button>
+                  </div>
+
+                  <div style={{ overflowY: "auto", height: "100%" }}>
+                    <Grid
+                      container
+                      wrap="unwrap"
+                      spacing={2}
+                      style={{ textAlign: "right" }}
+                    >
+                      <Grid item>
+                        <Avatar
+                          alt="ELON"
+                          src={imgLink}
+                          style={{ width: "50px", height: "50px" }}
+                        />
+                      </Grid>
+
+                      <Grid justifyContent="left" item xs zeroMinWidth>
+                        <h4
+                          style={{ margin: 0, textAlign: "left", color: "red" }}
+                        >
+                          He Buddy Man
+                        </h4>
+                        <p style={{ textAlign: "left" }}>
+                          Wow great image! Reminds me of the time when I was
+                          drawing theoretical chicken art!{" "}
+                        </p>
+                        <p style={{ textAlign: "left", color: "gray" }}>
+                          posted 54 minutes ago
+                        </p>
+                      </Grid>
+                    </Grid>
+
+                    <GridDivider />
+
+                    <Grid
+                      container
+                      wrap="unwrap"
+                      spacing={2}
+                      style={{ textALign: "right" }}
+                    >
+                      <Grid item>
+                        <Avatar
+                          alt="ELON"
+                          src={imgLink2}
+                          style={{ width: "50px", height: "50px" }}
+                        />
+                      </Grid>
+
+                      <Grid justifyContent="left" item xs zeroMinWidth>
+                        <h4
+                          style={{ margin: 0, textAlign: "left", color: "red" }}
+                        >
+                          He Buddy Man
+                        </h4>
+                        <p style={{ textAlign: "left" }}>
+                          I support your artwork. Consider adding your signature
+                          to your artworks.{" "}
+                        </p>
+                        <p style={{ textAlign: "left", color: "gray" }}>
+                          posted 6 hours ago
+                        </p>
+                      </Grid>
+                    </Grid>
+
+                    <GridDivider />
+
+                    <Grid
+                      container
+                      wrap="unwrap"
+                      spacing={2}
+                      style={{ textALign: "right" }}
+                    >
+                      <Grid item>
+                        <Avatar
+                          alt="ELON"
+                          src={imgLink3}
+                          style={{ width: "50px", height: "50px" }}
+                        />
+                      </Grid>
+
+                      <Grid justifyContent="left" item xs zeroMinWidth>
+                        <h4
+                          style={{ margin: 0, textAlign: "left", color: "red" }}
+                        >
+                          He Buddy Man
+                        </h4>
+                        <p style={{ textAlign: "left" }}>
+                          I believe if you added some lens flare this would be
+                          much better...{" "}
+                        </p>
+                        <p style={{ textAlign: "left", color: "gray" }}>
+                          posted 3 days ago
+                        </p>
+                      </Grid>
+                    </Grid>
+                  </div>
+                </div>
+              </Paper>
             </div>
-
-            <div className="LeavingFeedback-page-body">
-
-                <Grid container display="flex" flexDirection="row" justify="space-between">
-
-                    <Paper style={{ marginTop: 10, width: "auto", height: "auto", textAlign: "left" }}>
-                        <img src={state.image} width="600" height="600" alt="dynamo" />
-                    </Paper>
-
-                    
-
-                    <Paper style={{ padding: "10px 20px", marginTop: 10, width: "900px", height: "auto" }}>
-
-                        <h4>Critique this artwork. Be sure to follow the Critiquing Guidelines!</h4>
-
-                        <TextField id="standard-basic" label="Leave a Critique" padding="10px 20px" marginTop="10">
-                        </TextField>
-                        
-                        <Grid container wrap="unwrap" spacing={2} style={{ textALign: "right" }}>
-
-                            <Grid item>
-                                <Avatar alt="ELON" src={imgLink} style={{width:"50px", height:"50px"}}/>
-                            </Grid>
-
-                            <Grid justifyContent="left" item xs zeroMinWidth>
-                                <h4 style={{ margin: 0, textAlign: "left", color: "red" }}>He Buddy Man</h4>
-                                <p style={{ textAlign: "left" }}>
-                                    Wow great image! Reminds me of the time when I was drawing theoretical chicken art!{" "}
-                                </p>
-                                <p style={{ textAlign: "left", color: "gray" }}>
-                                    posted 54 minutes ago
-                                </p>
-                            </Grid>
-                        </Grid>
-
-                        <Divider variant="fullWidth" style={{ margin: "30px 0" }} />
-
-                        <Grid container wrap="unwrap" spacing={2} style={{ textALign: "right" }}>
-
-                            <Grid item>
-                                <Avatar alt="ELON" src={imgLink2} style={{width:"50px", height:"50px"}} />
-                            </Grid>
-
-                            <Grid justifyContent="left" item xs zeroMinWidth>
-                                <h4 style={{ margin: 0, textAlign: "left", color: "red" }}>He Buddy Man</h4>
-                                <p style={{ textAlign: "left" }}>
-                                    I support your artwork. Consider adding your signature to your artworks.{" "}
-                                </p>
-                                <p style={{ textAlign: "left", color: "gray" }}>
-                                    posted 6 hours ago
-                                </p>
-                            </Grid>
-                        </Grid>
-
-                        <Divider variant="fullWidth" style={{ margin: "30px 0" }} />
-
-                        <Grid container wrap="unwrap" spacing={2} style={{ textALign: "right" }}>
-
-                            <Grid item>
-                                <Avatar alt="ELON" src={imgLink3} style={{width:"50px", height:"50px"}}/>
-                            </Grid>
-
-                            <Grid justifyContent="left" item xs zeroMinWidth>
-                                <h4 style={{ margin: 0, textAlign: "left", color: "red" }}>He Buddy Man</h4>
-                                <p style={{ textAlign: "left" }}>
-                                    I believe if you added some lens flare this would be much better...{" "}
-                                </p>
-                                <p style={{ textAlign: "left", color: "gray" }}>
-                                    posted 3 days ago
-                                </p>
-                            </Grid>
-                        </Grid>
-
-                    </Paper>
-
-
-                </Grid>
-            </div>
-
+          </Grid>
         </div>
-    );
+      </div>
+    </div>
+  );
+}
+
+function GridDivider() {
+  return <Divider variant="fullWidth" style={{ margin: "10px 0 20px 0" }} />;
 }
