13dc94f1fc68fe1e4e33eb25f137362331faa074
shanedonburke <shanedonburke@gmail.com>
2021-04-23 13:39:06 -0400

    Fix missing import

--- src/Artistree/LeavingFeedback/LeavingFeedback.js
+++ src/Artistree/LeavingFeedback/LeavingFeedback.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import {
   TextField,
   Divider,
